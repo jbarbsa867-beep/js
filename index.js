c93aaca38971edbb52d4378b23339a7990944957
jbarbsa867-beep <jbarbsa867@gmail.com>
2026-06-09 09:20:35 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// Define a idade da pessoa
+let idade = 20;
+
+// Verifica se a idade é maior ou igual a 18
+if (idade >= 18) {
+    console.log("Maior de idade");
+} else {
+    console.log("Menor de idade");
+}

7e8631b3891077acfc1f0648744a982b0d124c6b
jbarbsa867-beep <jbarbsa867@gmail.com>
2026-06-09 19:14:36 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-// Define a idade da pessoa
-let idade = 20;
+/* Construa um algoritmo que solicita ao usuário
+   dois números e exiba qual deles é o maior.
+*/
 
-// Verifica se a idade é maior ou igual a 18
-if (idade >= 18) {
-    console.log("Maior de idade");
+// Importação da biblioteca prompt-sync
+const prompt = require('prompt-sync')();
+
+// Declaração de variáveis
+let num1 = parseFloat(prompt('Digite o primeiro número: '));
+let num2 = parseFloat(prompt('Digite o segundo número: '));
+
+// Comparando os números
+if (num1 > num2) {
+    console.log(`O maior número é ${num1}`);
+} else if (num2 > num1) {
+    console.log(`O maior número é ${num2}`);
 } else {
-    console.log("Menor de idade");
+    console.log('Os números são iguais');
 }
